8c540286de525fb34818d8945cb779cb440d10fe
Mohammad Sheakh <mohammad.sheakh@gmail.com>
2025-04-07 17:57:58 +0600

    updated

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,7 +3,8 @@ import path from 'path';
 
 // Initialize Firebase Admin SDK
  // const serviceAccount = require(''); // path.resolve(__dirname, '../../../../../')
-import serviceAccount from './aim-construction-firebase-adminsdk-fbsvc-5fd17ceb32.json'
+// import serviceAccount from './aim-construction-firebase-adminsdk-fbsvc-5fd17ceb32.json'
+import serviceAccount from './aim-construction-d864e-firebase-adminsdk-fbsvc-a79b881f12.json'
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
