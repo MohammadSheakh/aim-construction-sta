422764252965a62e7d541d2d78cdc6371ee249b7
Mohammad Sheakh <mohammad.sheakh@gmail.com>
2025-05-22 10:00:20 +0600

    repo added again

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,7 +4,7 @@ import path from 'path';
 // Initialize Firebase Admin SDK
  // const serviceAccount = require(''); // path.resolve(__dirname, '../../../../../')
 
-import serviceAccount from './aim-construction-firebase-adminsdk-fbsvc-5fd17ceb32.json'
+///////////////////////import serviceAccount from './aim-construction-firebase-adminsdk-fbsvc-5fd17ceb32.json'
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
